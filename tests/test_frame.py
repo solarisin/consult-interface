8641c36635cb4d1eebf0f37e78dce6debbd3aa13
Rob Longbottom <robert.longbottom@3db-labs.com>
2024-09-30 12:35:16 -0400

    Added reading of all frames individually

--- tests/test_frame.py
+++ tests/test_frame.py
@@ -42,9 +42,46 @@ def test_decode_response(caplog, data_registry):
     log.debug(f"  Sent Command: {test_data.command.hex(' ').upper()}")
     log.debug(f"  Capture File: {test_data.capture_file}")
 
-    with open(test_data.capture_file, 'rb') as file:
-        data = file.read(28)
-        frame_log.debug(data.hex(' ').upper())
+    with (open(test_data.capture_file, 'rb') as file):
+        # read until we find the frame start flag (0xFF), this is the command error check
+        command_check = bytearray()
+        while byte := file.read(1):
+            if len(byte) == 0:
+                raise ValueError("End of file reached before command check complete")
+            if byte == b'\xFF':
+                break
+            command_check.append(byte[0])
+        frame_log.debug(f"Command check response: {command_check.hex(' ').upper()}")
+
+        # TODO verify this response matches the command sent
+
+        first_frame = bytearray()
+        first_frame.append(0xFF)
+        # read the next byte, this is the size of the data in the frame
+        frame_size_byte = file.read(1)
+        if len(frame_size_byte) == 0:
+            raise ValueError("End of file reached before first frame size byte read")
+        frame_data_size = frame_size_byte[0]
+        frame_size = frame_data_size + 2
+        first_frame.append(frame_size)
+        log.debug(f"Frame Size: {frame_size}")
+
+        # read the rest of the first frame
+        first_frame_data = file.read(frame_data_size)
+        if len(first_frame_data) == 0:
+            raise ValueError("End of file reached before first frame data read")
+        first_frame.extend(first_frame_data)
+        frame_log.debug(f" 1: {first_frame.hex(' ').upper()}")
+
+        # read the rest of the frames in the file
+        n = 1
+        while frame := file.read(frame_size):
+            n+=1
+            if len(frame) != frame_size:
+                log.debug(f"End of file reached after {n} frames")
+                break
+            if n < 100:
+                frame_log.debug(f"{n:2}: {frame.hex(' ').upper()}")
 
     # consult = MockConsultSerial(port, response_playback_file)
     # consult.initialize_ecu()
